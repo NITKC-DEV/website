764975e37fb4f0d1d9509f41ff2beda73b6fe44d
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-06-17 22:03:26 +0900

    renewal

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "NITKC22s.github.io" : "./",
+  plugins: [react()],
+});
