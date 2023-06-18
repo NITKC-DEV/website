9c1fea50eac39802c2f1819689db177e7000afbf
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-06-19 05:36:37 +0900

    うごけ

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/modules/Header.tsx";
 import NotFound from "./components/pages/status/NotFound.tsx";
 import Home from "./components/pages/Home.tsx";
-import Mechanical from "./components/pages/department/mechanical.tsx";
-import Electrical from "./components/pages/department/electrical.tsx";
-import ControlEngineering from "./components/pages/department/control_engineering.tsx";
-import Information from "./components/pages/department/information.tsx";
-import CivilEngineering from "./components/pages/department/civil_engineering.tsx";
+import Mechanical from "./components/pages/departments/mechanical.tsx";
+import Electrical from "./components/pages/departments/electrical.tsx";
+import ControlEngineering from "./components/pages/departments/control_engineering.tsx";
+import Information from "./components/pages/departments/information.tsx";
+import CivilEngineering from "./components/pages/departments/civil_engineering.tsx";
 
 function App() {
     const theme = createTheme({
@@ -35,28 +35,28 @@ function App() {
                         <Link to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/department/mechanical">機械工学科</Link>
+                        <Link to="/mechanical">機械工学科</Link>
                     </li>
                     <li>
-                        <Link to="/department/electrical">電気電子工学科</Link>
+                        <Link to="/electrical">電気電子工学科</Link>
                     </li>
                     <li>
-                        <Link to="/department/control_engineering">電子制御工学科</Link>
+                        <Link to="/control_engineering">電子制御工学科</Link>
                     </li>
                     <li>
-                        <Link to="/department/information">情報工学科</Link>
+                        <Link to="/information">情報工学科</Link>
                     </li>
                     <li>
-                        <Link to="/department/civil_engineering">環境都市工学科</Link>
+                        <Link to="/civil_engineering">環境都市工学科</Link>
                     </li>
                 </ul>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/department/mechanical" element={<Mechanical />} />
-                    <Route path="/department/electrical" element={<Electrical />} />
-                    <Route path="/department/control_engineering" element={<ControlEngineering />} />
-                    <Route path="/department/information" element={<Information />} />
-                    <Route path="/department/civil_engineering" element={<CivilEngineering />} />
+                    <Route path="/mechanical" element={<Mechanical />} />
+                    <Route path="/electrical" element={<Electrical />} />
+                    <Route path="/control_engineering" element={<ControlEngineering />} />
+                    <Route path="/information" element={<Information />} />
+                    <Route path="/civil_engineering" element={<CivilEngineering />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </ThemeProvider>
