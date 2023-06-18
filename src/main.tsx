8cf910d3beea6090178e726169ec2e82305c6476
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-06-19 04:49:38 +0900

    404対策第5弾(ロールバック処理)

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 
-const ROUTER_BASENAME = process.env.NODE_ENV === "development" ? "/" : "/nitkc22s.github.io";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <BrowserRouter basename={ROUTER_BASENAME}>
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
