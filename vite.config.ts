ef3675076a126c7220d8a61c664d9f834f4a450c
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-06-22 12:49:18 +0900

    トレイリングスラッシュを取り除く(第14弾)
    ・おまえか

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.GITHUB_PAGES ? "NITKC22s.github.io" : "./",
+    base: "https://nitkc22s.pages.dev/",
     plugins: [react()],
     build: {
         outDir: "dist",
